01e0d886b679a5d879ed2ff4e5ab459561cbf02a
Maxim Glukhov <andromeda31388@gmail.com>
2023-02-13 17:47:43 +0500

    add database and tables creation

--- app/database.php
+++ app/database.php
@@ -14,11 +14,17 @@ class Database{
     }
 
     public function connect() {
-        $conn_str = "mysql:host=" . $this->host . ";dbname=" . $this->dbname;
+        $conn_str = "mysql:host=" . $this->host;
 
         try {
             $conn = new PDO($conn_str, $this->user, $this->pass);
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $conn->exec("CREATE DATABASE IF NOT EXISTS urlshortener");
+            $conn->exec("use $this->dbname");
+            $sql1 = "create table if not exists users (id int(11) primary key, email varchar(100), password varchar(100));";
+            $conn->exec($sql1);
+            $sql2 = "create table if not exists urls (id int(11) primary key, short_url varchar(300), orig_url varchar(2000), clicks integer(6), user_id integer, foreign key (user_id) references users(id));";
+            $conn->exec($sql2);
 
             return $conn;
         } catch (PDOException $e) {
